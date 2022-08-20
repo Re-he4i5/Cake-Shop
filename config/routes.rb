d4882198ceca48a641e651145463b9010eec1cec
Re-he4i5 <kuronekoryohei@gmail.com>
2022-08-20 11:58:50 +0000

    [add]account_manage

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,17 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'homes#top', as: 'top'
+    resources :customers, only: [:index]
   end
 
   scope module: :public do
     root 'homes#top'
     get 'about' => 'homes#about' ,as: 'about'
-    get 'customers/my_page' => 'customers#show', as: 'my_page'
+    get 'customers/my_page' => 'customers#show', as: 'mypage'
+    get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customers/information' => 'customers#update', as: 'update_information'
+
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
   end
 end
